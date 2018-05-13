d8f74e607bf76473365794d663df7ded7770de65
hyf <hyf_109@163.com>
2018-05-13 22:34:23 +0800

    tianjia

--- src/main/java/readinglist/controller/ReadingListController.java
+++ src/main/java/readinglist/controller/ReadingListController.java
@@ -1,13 +1,37 @@
 package readinglist.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import readinglist.entity.Book;
 import readinglist.service.ReadingListRepository;
 
+import java.util.List;
+
+@Controller
+@RequestMapping("/readlingList")
 public class ReadingListController {
     private ReadingListRepository readingListRepository;
-
+    @Autowired
     public ReadingListController(ReadingListRepository readingListRepository) {
         this.readingListRepository = readingListRepository;
     }
-
+    @RequestMapping("list")
+    public String readersBooks(@PathVariable("reader") String reader, Model model){
+        List<Book> readingList = readingListRepository.findByReader(reader);
+        if(readingList!=null){
+            model.addAttribute("books",readingList);
+        }
+        return "readingList";
+    }
+    @RequestMapping("/add")
+    public String addToReadingList(@PathVariable("reader") String reader, Book book){
+        book.setReader(reader);
+        readingListRepository.save(book);
+        return "redirect:/readingList/{reader}";
+    }
 
 }
